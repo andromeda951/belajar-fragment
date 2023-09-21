74ebc871824d3e621b0b73a00e8898bc07c63c89
Andromeda <andromeda951@gmail.com>
2023-09-21 08:02:47 +0700

    Create and display fragments

--- app/src/main/java/com/andromeda/belajarfragment/MainActivity.kt
+++ app/src/main/java/com/andromeda/belajarfragment/MainActivity.kt
@@ -7,5 +7,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val fragment1 = BlankFragment1()
+        val fragment = supportFragmentManager.findFragmentByTag(BlankFragment1::class.java.simpleName)
+
+        if (fragment !is BlankFragment1) {
+            supportFragmentManager.beginTransaction()
+                .add(R.id.container, fragment1, BlankFragment1::class.java.simpleName)
+                .commit()
+        }
+
+
     }
 }
